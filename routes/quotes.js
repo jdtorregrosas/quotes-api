a11165b8d9f700ff9b1accc12d9551e7e9c7f057
Julian David <julian.torregrosa@banlinea.com>
2017-09-29 14:28:35 -0500

    Create get and post of quotes

---
+++ routes/quotes.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var admin = require("firebase-admin");
+const uuidv4 = require('uuid/v4');
+
+var serviceAccount = require("../quotes-60ae9-firebase-adminsdk-advrp-5868a29c5f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://quotes-60ae9.firebaseio.com/"
+});
+
+var db = admin.database();
+var ref = db.ref("quotes");
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  var id = uuidv4()
+  var quoteRequest = req.body
+  ref.child(id).set(quoteRequest);
+  return res.json(quoteRequest);
+});
+
+router.get('/', function(req, res, next) {
+  new Promise((resolve, reject) => {
+    ref.on("value", function(snapshot) {
+      resolve(snapshot)
+
+    }, function (errorObject) {
+      reject(errorObject)
+    });
+  }).then((success) => {
+    res.json(success)
+  }).catch((err)=>{
+    res.status(401).json(err)
+  })
+});
+
+module.exports = router;
